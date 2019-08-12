4c34700818846ff88553d43f16c8a32393df7fe1
vimal Raveendran <vimalraveendra@gmail.com>
2019-08-12 10:56:54 +0200

    Updates

---
+++ precache-manifest.bb505ef1842d105a1a4a0e70a42deb56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6cb0b6ccee155f88d28a9deefca776",
+    "url": "/Persons-list/index.html"
+  },
+  {
+    "revision": "079438d05d36366c429f",
+    "url": "/Persons-list/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "46cc83375d4c6644a404",
+    "url": "/Persons-list/static/css/main.148d42ba.chunk.css"
+  },
+  {
+    "revision": "079438d05d36366c429f",
+    "url": "/Persons-list/static/js/2.c56086cb.chunk.js"
+  },
+  {
+    "revision": "46cc83375d4c6644a404",
+    "url": "/Persons-list/static/js/main.caf955ff.chunk.js"
+  },
+  {
+    "revision": "ff7c3447defa1083b092",
+    "url": "/Persons-list/static/js/runtime~main.4858a2b5.js"
+  },
+  {
+    "revision": "df868691bca38b437758eab2262299f2",
+    "url": "/Persons-list/static/media/Lintsec.df868691.woff"
+  }
+]);
