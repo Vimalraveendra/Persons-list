6c5e82c397f5350ea7ed2835f56ddd4fca5c0ea9
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-15 22:10:06 +0100

    updated App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,25 @@
-import React ,{Component}from 'react';
-import PersonsList from '../components/PersonsList';
+import React, { Component } from "react";
+import PersonsList from "../components/PersonsList";
 
-
-
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-    state = {
-      persons:[]
-    }
+  state = {
+    persons: []
+  };
 
-    componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=>response.json())
-      .then(users=>this.setState({persons:users}))
-
-    }
- render(){
-  return (
-    <div >
-      <h1 className='tc'>Personal List</h1>
-       <PersonsList  personsArray={this.state.persons}/>
-     
-     
-    </div>
-  );
-}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ persons: users }));
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="tc">Personal List</h1>
+        <PersonsList personsArray={this.state.persons} />
+      </div>
+    );
+  }
 }
 export default App;
-
